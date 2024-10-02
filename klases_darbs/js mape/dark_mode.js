76085d9184745daa89dc85204556d886a15f2887
Ralph <rm00457@rvt.lv>
2024-10-02 13:20:07 +0300

    Dark mode fixed

--- klases_darbs/js mape/dark_mode.js
+++ klases_darbs/js mape/dark_mode.js
@@ -5,6 +5,7 @@ const body = document.body;
 const isDarkMode = localStorage.getItem('darkMode') === 'true';
 if (isDarkMode) {
     body.classList.add('dark-mode');
+    toggleButton.textContent = 'Light Mode';
 }
 
 toggleButton.addEventListener('click', () => {
@@ -13,4 +14,10 @@ toggleButton.addEventListener('click', () => {
     // Store the user's preference for dark mode in local storage
     const darkModeEnabled = body.classList.contains('dark-mode');
     localStorage.setItem('darkMode', darkModeEnabled);
+
+    if (darkModeEnabled) {
+        toggleButton.textContent = 'Light Mode'; // Change to "Light Mode" when dark mode is active
+    } else {
+        toggleButton.textContent = 'Dark Mode';  // Change to "Dark Mode" when dark mode is off
+    }
 });
