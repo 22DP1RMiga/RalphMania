01509f323caf02f3479362b5ad6436bb78280c3d
Adam Merkel <rm00457@rvt.lv>
2024-10-25 16:19:10 +0300

    language improvement

--- klases_darbs/js mape/dark_mode.js
+++ klases_darbs/js mape/dark_mode.js
@@ -3,9 +3,18 @@ const body = document.body;
 
 // Load dark mode preference from local storage if exists
 const isDarkMode = localStorage.getItem('darkMode') === 'true';
+const selectedLang = document.getElementById('language-dropdown').value;
+
+function setDarkModeText(lang) {
+    const text = isDarkMode ? translations[lang].lightmode : translations[lang].darkmode;
+    toggleButton.textContent = text;
+}
+
+// Initial setup for dark mode
 if (isDarkMode) {
     body.classList.add('dark-mode');
     toggleButton.textContent = 'Light Mode';
+    setDarkModeText(selectedLang);
 }
 
 toggleButton.addEventListener('click', () => {
--- klases_darbs/js mape/language.js
+++ klases_darbs/js mape/language.js
@@ -20,11 +20,21 @@ const translations = {
         searchbar: "Search here...",
         searchbutton: "Go",
         originTitle: "ORIGIN",
-        originText: "This website was made for a task purpose to make at least 3 tickets. There are 4 card-shaped tickets that will lead to YouTube videos about them.",
+        originText: "This website was made for a task purpose to make at least 3 tickets.\n\nThere are 4 card-shaped tickets that will lead to YouTube videos about them.",
         howToTitle: "HOW TO USE THE WEBSITE",
-        howToText: "If you want to search for something, search 'Apple' for instance to see it being highlighted. Switch to Dark Mode if you wish to have a darker screen and style."
-        // Add more translations for other texts
+        howToText: "If you want to search for something, search 'Apple' for instance to see it being highlighted.\n\nSwitch to Dark Mode if you wish to have a darker screen and style.",
+        apple: "Apple",
+        banana: "Banana",
+        lychee: "Lychee",
+        chickentenders: "Chicken tenders",
+        appletext: "Red, just not the green Newton-discovered apple",
+        bananatext: "Yellow, monkeys' favorite fruit",
+        lycheetext: "A very juicy and normal fruit, kind of exotic",
+        tendertext: "Mmmm.. chicken fingers",
+        darkmode: "Dark Mode",
+        lightmode: "Light Mode"
     },
+
     lv: {
         home: "Mājas",
         about: "Par",
@@ -34,11 +44,21 @@ const translations = {
         searchbar: "Meklē šeit...",
         searchbutton: "Meklēt",
         originTitle: "IZCELSME",
-        originText: "Šī mājaslapa tika izveidota uzdevuma mērķiem, lai izveidotu vismaz 3 biļetes. Ir 4 kartīšu formas biļetes, kas novadīs uz YouTube videoklipiem par tām.",
+        originText: "Šī mājaslapa tika izveidota uzdevuma mērķiem, lai izveidotu vismaz 3 biļetes.\n\nIr 4 kartīšu formas biļetes, kas novadīs uz YouTube videoklipiem par tām.",
         howToTitle: "KĀ LIETOT MĀJASLAPU",
-        howToText: "Ja vēlaties meklēt kaut ko, meklējiet 'Apple', piemēram, lai to izceltu. Pārejiet uz tumšo režīmu, ja vēlaties, lai ekrāns un stils būtu tumšāks."
-        // Add more translations for Latvian
+        howToText: "Ja vēlaties meklēt kaut ko, meklējiet 'Ābols', piemēram, lai to izceltu.\n\nPārejiet uz tumšo režīmu, ja vēlaties, lai ekrāns un stils būtu tumšāks.",
+        apple: "Ābols",
+        banana: "Banāns",
+        lychee: "Līčijs",
+        chickentenders: "Vistu strēmeles",
+        appletext: "Sarkarns, tikai ne zaļš Ņūtona atklātais ābols",
+        bananatext: "Dzeltens, pērtiķiem mīļākais auglis",
+        lycheetext: "Ļoti sulīgs un normāls auglis, diezgan eksotisks",
+        tendertext: "Mmmm.. vistu pirkstiņi",
+        darkmode: "Tumšais režīms",
+        lightmode: "Gaišais režīms"
     },
+
     ru: {
         home: "Дом",
         about: "Про",
@@ -48,11 +68,21 @@ const translations = {
         searchbar: "Искать здесь...",
         searchbutton: "Давай",
         originTitle: "ИСТОЧНИК",
-        originText: "Этот сайт был создан с целью сделать как минимум 3 билета. Есть 4 билета в форме карточек, которые ведут к видеороликам о них на YouTube.",
-        howToTitle: "КАК ПsОЛЬЗОВАТЬСЯ ВЕБ-САЙТОМ",
-        howToText: "Если вы хотите что-то найти, введите, например, «Apple», чтобы увидеть, что оно выделено. Переключитесь в темный режим, если вы хотите иметь более темный экран и стиль."
-        // Add more translations for Russian
+        originText: "Этот сайт был создан с целью сделать как минимум 3 билета.\n\nЕсть 4 билета в форме карточек, которые ведут к видеороликам о них на YouTube.",
+        howToTitle: "КАК ПОЛЬЗОВАТЬСЯ ВЕБ-САЙТОМ",
+        howToText: "Если вы хотите что-то найти, введите, например, «Яблоко», чтобы увидеть, что оно выделено.\n\nПереключитесь в темный режим, если вы хотите иметь более темный экран и стиль.",
+        apple: "Яблоко",
+        banana: "Банан",
+        lychee: "Личи",
+        chickentenders: "Куриные тендеры",
+        appletext: "Красное, но не зеленое яблоко, открытое Ньютоном",
+        bananatext: "Желтый, любимый фрукт обезьян",
+        lycheetext: "Очень сочный и нормальный фрукт, какая-то экзотика",
+        tendertext: "Мммм.. куриные палочки",
+        darkmode: "Тёмный режим",
+        lightmode: "Светлый режим"
     },
+
     de: {
         home: "Heim",
         about: "Um",
@@ -62,36 +92,41 @@ const translations = {
         searchbar: "Suchen Sie hier...",
         searchbutton: "Gehen",
         originTitle: "HERKUNFT",
-        originText: "Diese Website wurde mit dem Ziel erstellt, mindestens 3 Tickets zu erstellen. Es gibt 4 kartenförmige Tickets, die zu YouTube-Videos darüber führen.",
+        originText: "Diese Website wurde mit dem Ziel erstellt, mindestens 3 Tickets zu erstellen.\n\nEs gibt 4 kartenförmige Tickets, die zu YouTube-Videos darüber führen.",
         howToTitle: "SO BENUTZEN SIE DIE WEBSITE",
-        howToText: "Wenn Sie nach etwas suchen möchten, suchen Sie beispielsweise nach „Apple“, um zu sehen, dass es hervorgehoben wird. Wechseln Sie in den Dunkelmodus, wenn Sie einen dunkleren Bildschirm und Stil wünschen."
-        // Add more translations for German
+        howToText: "Wenn Sie nach etwas suchen möchten, suchen Sie beispielsweise nach „Apfel“, um zu sehen, dass es hervorgehoben wird.\n\nWechseln Sie in den Dunkelmodus, wenn Sie einen dunkleren Bildschirm und Stil wünschen.",
+        apple: "Apfel",
+        banana: "Banane",
+        lychee: "Litschi",
+        chickentenders: "Hähnchenfilets",
+        appletext: "Rot, nur nicht der grüne, von Newton entdeckte Apfel",
+        bananatext: "Gelb, die Lieblingsfrucht der Affen",
+        lycheetext: "Eine sehr saftige und normale Frucht, irgendwie exotisch",
+        tendertext: "Mmmm.. Hähnchenstäbchen",
+        darkmode: "Dunkler Modus",
+        lightmode: "Lichtmodus"
     }
 };
 
 // Translation function
 function updateLanguage(lang) {
     document.querySelectorAll('.translate').forEach((element) => {
-        const key = element.parentElement.getAttribute('data-translate');
+        const key = element.getAttribute('data-translate'); // Corrected attribute reference
         if (translations[lang][key]) {
             element.textContent = translations[lang][key];
+            element.innerHTML = translations[lang][key].replace(/\n\n/g, '<br><br>');
         }
     });
 
-    // Update other texts
-    document.querySelector('#origin-section h1').textContent = translations[lang].originTitle;
-    document.querySelector('#origin-section p').textContent = translations[lang].originText;
-    document.querySelector('#how-to-section h1').textContent = translations[lang].howToTitle;
-    document.querySelector('#how-to-section p').textContent = translations[lang].howToText;
-    document.querySelector('#search-input').textContent = translations[lang].searchbar;
-    document.querySelector('#search-button').textContent = translations[lang].searchbutton + ' <i class="fa-solid fa-magnifying-glass"></i>';
+    // Update input placeholders and button text separately
+    document.querySelector('#search-input').placeholder = translations[lang].searchbar;
+    document.querySelector('#search-button').innerHTML = translations[lang].searchbutton + ' <i class="fa-solid fa-magnifying-glass"></i>';
 }
 
 // Dropdown functionality for languages
 document.getElementById('language-dropdown').addEventListener('change', (event) => {
     updateLanguage(event.target.value);
 });
-
 
 // Default language
 window.onload = () => {
