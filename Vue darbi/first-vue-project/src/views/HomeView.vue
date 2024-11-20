ba4ed7456d1c00a9ee07d8a255ec92d60b3c48ab
Adam Merkel <rm00457@rvt.lv>
2024-11-20 12:09:15 +0200

    Pievienots Vue projekts

---
+++ Vue darbi/first-vue-project/src/views/HomeView.vue
@@ -0,0 +1,52 @@
+<template>
+  <main>
+
+    <h1>{{ score }}</h1>
+
+    <div class="cookie">
+      <img class="cookie"
+      :class="{ bounce: isBouncing }" 
+      src="https://img.freepik.com/free-photo/chocolate-chip-cookies-isolated-white-background-ai-generative_123827-24072.jpg?semt=ais_hybrid"
+      @click="handleClickEvent"
+      >
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      score: 0,
+    }
+  },
+
+  methods: {
+    handleClickEvent() {
+      this.score += 1;
+    }
+  }
+}
+</script>
+
+<!-- stils tikai šim failam. Citos vārdos: private style -->
+<style scoped>
+* {
+  font-family: cursive;
+}
+
+main {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  width: 400px;
+  object-fit: cover;
+}
+
+</style>
