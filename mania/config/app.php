4bde28aaeaa52c0458cb6b4969873520095bd3ba
Ralph <rm00457@rvt.lv>
2025-03-04 14:18:51 +0200

    Changed app icon

--- mania/config/app.php
+++ mania/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'RalphMania'),
 
     /*
     |--------------------------------------------------------------------------
@@ -53,6 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+//    'url' => env('APP_URL', 'https://ralphmania.com'),
 
     /*
     |--------------------------------------------------------------------------
