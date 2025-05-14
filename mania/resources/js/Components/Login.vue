52184cae984dbcb930e89eed09e3e7715c8247fc
Ralph <rm00457@rvt.lv>
2025-05-14 23:29:59 +0300

    Huge improvements (Login/Register)

--- mania/resources/js/Components/Login.vue
+++ mania/resources/js/Components/Login.vue
@@ -27,7 +27,7 @@
 
                 <button @click="submitLogin">Log In</button>
                 <a href="/register"><p class="switch-link">Don't have an account? Register</p></a>
-<!--                <p v-if="errorMessage" class="error">{{ errorMessage }}</p>-->
+                <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
             </div>
         </div>
     </main>
@@ -36,163 +36,64 @@
 
 <style scoped>
     .form-container {
-        height: 100vh;
-        width: 100%;
         display: flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
-        background-color: #420d65;
-        column-gap: 30px;
+        padding: 20px;
     }
 
-    .link:hover {
-        cursor: pointer;
+    .RoltonsLV_Icon {
+        max-width: 150px;
+        padding: 10px;
+        justify-self: center;
     }
-    .form{
-        position: absolute;
-        max-width: 430px;
-        width: 100%;
-        padding: 30px;
-        border-radius: 6px;
-        background: #FFF;
-    }
-    .form.signup{
-        opacity: 0;
-        pointer-events: none;
-    }
-    .forms.show-signup .form.signup{
-        opacity: 1;
-        pointer-events: auto;
-    }
-    .forms.show-signup .form.login{
-        opacity: 0;
-        pointer-events: none;
-    }
-    header{
-        font-size: 28px;
-        font-weight: 600;
-        color: #232836;
+
+    .auth-box {
+        background: linear-gradient(to bottom, rgb(229, 112, 112), rgb(143, 69, 69));
+        border-radius: 10px;
+        border: 2px solid black;
+        width: 400px;
+        padding: 20px;
         text-align: center;
+        box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.5);
     }
-    form{
-        margin-top: 30px;
+    .auth-box input {
+        width: 90%;
+        padding: 10px;
+        margin: 10px 0;
+        border-radius: 10px;
+        border: 1px solid black;
     }
-    .form .field{
-        position: relative;
-        height: 50px;
-        width: 100%;
-        margin-top: 20px;
-        border-radius: 6px;
+    .auth-box h2 {
+        font-weight: bold;
+        color: white;
     }
-    .field input,
-    .field button{
-        height: 100%;
-        width: 100%;
+
+    button {
+        margin: 10px;
+        padding: 8px 15px;
+        background-color: firebrick;
+        color: white;
         border: none;
-        font-size: 16px;
-        font-weight: 400;
-        border-radius: 6px;
-    }
-    .field input{
-        outline: none;
-        padding: 0 15px;
-        border: 1px solid#CACACA;
-    }
-    .field input:focus{
-        border-bottom-width: 2px;
-    }
-    .eye-icon{
-        position: absolute;
-        top: 50%;
-        right: 10px;
-        transform: translateY(-50%);
-        font-size: 18px;
-        color: #8b8b8b;
-        cursor: pointer;
-        padding: 5px;
-    }
-    .field button{
-        color: #fff;
-        background-color: #420d65;
-        transition: all 0.3s ease;
+        border-radius: 5px;
         cursor: pointer;
     }
-    .field button:hover{
-        background-color: #420d65;
+    button:hover {
+        background-color: darkred;
     }
-    .form-link{
-        text-align: center;
+    .switch-link {
         margin-top: 10px;
-    }
-    .form-link span,
-    .form-link a{
-        font-size: 14px;
-        font-weight: 400;
-        color: #232836;
-    }
-    .form a{
-        color: #420d65;
-        text-decoration: none;
-    }
-    .form-content a:hover{
+        color: white;
         text-decoration: underline;
+        cursor: pointer;
     }
-    .line{
-        position: relative;
-        height: 1px;
-        width: 100%;
-        margin: 36px 0;
-        background-color: #d4d4d4;
+    .switch-link:hover {
+        color: lightgray;
     }
-    .line::before{
-        content: 'Or';
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background-color: #FFF;
-        color: #8b8b8b;
-        padding: 0 15px;
-    }
-    .media-options a{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    a.facebook{
-        color: #fff;
-        background-color: #420d65;
-    }
-    a.facebook .facebook-icon{
-        height: 28px;
-        width: 28px;
-        color: #420d65;
-        font-size: 20px;
-        border-radius: 50%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: #fff;
-    }
-    .facebook-icon,
-    img.google-img{
-        position: absolute;
-        top: 50%;
-        left: 15px;
-        transform: translateY(-50%);
-    }
-    img.google-img{
-        height: 20px;
-        width: 20px;
-        object-fit: cover;
-    }
-    a.google{
-        border: 1px solid #CACACA;
-    }
-    a.google span{
-        font-weight: 500;
-        opacity: 0.6;
-        color: #232836;
+    .error {
+        color: red;
+        margin-top: 10px;
     }
 
     @media screen and (max-width: 400px) {
--- mania/resources/js/Components/Register.vue
+++ mania/resources/js/Components/Register.vue
@@ -33,60 +33,61 @@ const submitForm = () => {
 
 
 <style scoped>
-.form-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 20px;
-}
-.RoltonsLV_Icon {
-    max-width: 150px;
-    padding: 10px;
-}
-.auth-box {
-    background: linear-gradient(to bottom, rgb(229, 112, 112), rgb(143, 69, 69));
-    border-radius: 10px;
-    border: 2px solid black;
-    width: 400px;
-    padding: 20px;
-    text-align: center;
-    box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.5);
-}
-.auth-box input {
-    width: 90%;
-    padding: 10px;
-    margin: 10px 0;
-    border-radius: 10px;
-    border: 1px solid black;
-}
-.auth-box h2 {
-    font-weight: bold;
-    color: white;
-}
-button {
-    margin: 10px;
-    padding: 8px 15px;
-    background-color: firebrick;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-}
-button:hover {
-    background-color: darkred;
-}
-.switch-link {
-    margin-top: 10px;
-    color: white;
-    text-decoration: underline;
-    cursor: pointer;
-}
-.switch-link:hover {
-    color: lightgray;
-}
-.error {
-    color: red;
-    margin-top: 10px;
-}
+    .form-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 20px;
+    }
+    .RoltonsLV_Icon {
+        max-width: 150px;
+        padding: 10px;
+        justify-self: center;
+    }
+    .auth-box {
+        background: linear-gradient(to bottom, rgb(229, 112, 112), rgb(143, 69, 69));
+        border-radius: 10px;
+        border: 2px solid black;
+        width: 400px;
+        padding: 20px;
+        text-align: center;
+        box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.5);
+    }
+    .auth-box input {
+        width: 90%;
+        padding: 10px;
+        margin: 10px 0;
+        border-radius: 10px;
+        border: 1px solid black;
+    }
+    .auth-box h2 {
+        font-weight: bold;
+        color: white;
+    }
+    button {
+        margin: 10px;
+        padding: 8px 15px;
+        background-color: firebrick;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+    button:hover {
+        background-color: darkred;
+    }
+    .switch-link {
+        margin-top: 10px;
+        color: white;
+        text-decoration: underline;
+        cursor: pointer;
+    }
+    .switch-link:hover {
+        color: lightgray;
+    }
+    .error {
+        color: red;
+        margin-top: 10px;
+    }
 </style>
